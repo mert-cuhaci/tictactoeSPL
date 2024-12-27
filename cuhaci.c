eaebab1925eac25a0be652ba35842055335e9150
mert-cuhaci <mertcuhaci1@gmail.com>
2024-12-27 14:23:02 +0100

    Update cuhaci.c

--- cuhaci.c
+++ cuhaci.c
@@ -1,15 +1,63 @@
 #include <stdio.h>
 
 //preprocessor macros
+#define SIZE 3 	//3,5 oder 7
+#define PLAYERS 2 	// 2 oder 3
+#define TIME 0 		//0, 10 oder 15
+#define BLOCKED 0 	//1 oder 0
+#define JOKER 0 	//1 oder 0
+#define LEN 3		//3,4 oder 5
 
-#define size 3x3 	//3,5 oder 7
-#define players 2 	// 2 oder 3
-#define time 0 		//0, 10 oder 15
-#define blocked 0 	//1 oder 0
-#define joker 0 	//1 oder 0
-#define len 3		//3,4 oder 5
+//global variables
+int turn = 0
 
-char board = 
+#ifdef SIZE == 3
+char board[3][3] =    {	{'.', '.', '.'} , 
+						{'.', '.', '.'} , 
+						{'.', '.', '.'}};
+#endif
+
+#ifdef SIZE == 5
+char board[3][3] =    {	{'.', '.', '.', '.', '.'} , 
+						{'.', '.', '.', '.', '.'} , 
+						{'.', '.', '.', '.', '.'} , 
+						{'.', '.', '.', '.', '.'} , 
+						{'.', '.', '.', '.', '.'}}
+#endif
+
+#ifdef SIZE == 7
+char board[3][3] =    {	{'.','.','.','.','.', '.', '.'} , 
+						{'.','.','.','.','.', '.', '.'} , 
+						{'.','.','.','.','.', '.', '.'} ,
+						{'.','.','.','.','.', '.', '.'} ,
+						{'.','.','.','.','.', '.', '.'} ,
+						{'.','.','.','.','.', '.', '.'} ,
+						{'.','.','.','.','.', '.', '.'}};
+#endif
+
+char turnToChar(int t){
+	if(t==0){
+		return 'X';
+	}
+	if(t==1){
+		return 'O';
+	}
+	if(t==2){
+		return 'P';
+	}
+	return 'a';
+}
+
+//displays any necessary information
+void printBoard(){
+	#ifdef TIME
+	printf("Time Limit: %d\n", TIME);
+	#endif
+	printf("Turn: %c\n", turnToChar());
+	for(int i=0;i<SIZE;i++){
+		for(int j=0;j<SIZE;j++){
+			printf("")
+	
 
 
 
