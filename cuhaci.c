0b575bbaa11c05a020cb86ee062305ba0d77275a
mert-cuhaci <mertcuhaci1@gmail.com>
2024-12-27 13:29:45 +0100

    first commit
    Restarted the project with c

---
+++ cuhaci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+//preprocessor macros
+
+#define size 3x3 	//3,5 oder 7
+#define players 2 	// 2 oder 3
+#define time 0 		//0, 10 oder 15
+#define blocked 0 	//1 oder 0
+#define joker 0 	//1 oder 0
+#define len 3		//3,4 oder 5
+
+char board = 
+
+
+
+
+#ifdef
+
+int checkFeatures(){
+	#ifdef VALID
+	printf("Feature selection is valid.");
+	printf()
+	#else
+	
+	#endif
+}
+
+
+
+int main(void) {
+    checkFeatures();
+    
+    
+    
+    
+    // This is the main function of the program
+    printf("Hello, World!\n");
+
+    return 0;
+}
